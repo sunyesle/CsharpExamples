8e23796ed9a2c4cfadc2c9782aaf790544559eed
sunyesle <sunyesle@gmail.com>
2018-12-26 21:23:02 +0900

    ch12 예외 처리하기

---
+++ CsharpExamples/MainApp_12_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class MainApp_12_1
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine("종료");
+        }
+    }
+}
---
+++ CsharpExamples/MainApp_12_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class MainApp_12_2
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("예외가 발생했습니다 : {0}", e.Message);
+            }
+
+            Console.WriteLine("종료");
+        }
+    }
+}
