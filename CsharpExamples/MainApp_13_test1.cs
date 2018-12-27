be5a4bf74c88cb9c5103095158061444bf302068
sunyesle <sunyesle@gmail.com>
2018-12-27 22:01:12 +0900

    13장 연습문제

---
+++ CsharpExamples/MainApp_13_test1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExamples
+{
+    delegate int MyDelegate(int a, int b);
+
+    class MainApp_13_test1
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate Callback;
+
+            Callback = delegate (int a, int b)
+            {
+                return a + b;
+            };
+            Console.WriteLine(Callback(3,4));
+
+            Callback = delegate (int a, int b)
+            {
+                return a - b;
+            };
+            Console.WriteLine(Callback(7,5));
+        }
+    }
+}
---
+++ CsharpExamples/MainApp_13_test2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExamples
+{
+    delegate void Mydelegate(string message);
+
+    class MyNotifier
+    {
+        public event Mydelegate SomethingHappened;
+        public void DoSomething(int num)
+        {
+            if (num == 30)
+                SomethingHappened(String.Format("축하합니다! {0}번째 고객 이벤트에 당첨되셨습니다.", num));
+        }
+    }
+
+    class MainApp_13_test2
+    {
+        static public void MyHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            MyNotifier notifier = new MyNotifier();
+            notifier.SomethingHappened += new Mydelegate(MyHandler);
+
+            for (int i = 0; i < 40; i++)
+            {
+                notifier.DoSomething(i);
+            }
+
+        }
+    }
+}
