d3f3263d5a09a56bce13155c51738723e8c89f07
sunyesle <sunyesle@gmail.com>
2018-12-28 15:18:18 +0900

    14장 연습문제

---
+++ CsharpExamples/MainApp_14_test2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExamples
+{
+    class MainApp_14_test2
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 11, 22, 33, 44, 55 };
+
+            foreach (int a in array)
+            {
+                Action action = () => Console.WriteLine(a * a);
+                action.Invoke();
+            }
+        }
+    }
+}
