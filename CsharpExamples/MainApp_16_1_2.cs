c1b072f59d92d69ca608bcb9f725dd901c4dd079
sunyesle <sunyesle@gmail.com>
2018-12-31 19:09:43 +0900

    ch16 리플렉션과 애트리뷰트

---
+++ CsharpExamples/MainApp_16_1_2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExamples    // 리플렉션 DynamicInstance
+{
+    class Profile
+    {
+        private string name;
+        private string phone;
+
+        public Profile()
+        {
+            name = ""; phone = "";
+        }
+
+        public Profile (string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("{0}, {1}", name , phone);
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+    }
+
+    class MainApp_16_1_2
+    {
+        static void Main(string[] args)
+        {
+            Type type = Type.GetType("CsharpExamples.Profile"); //네임스페이스를 포함하는 전체이름을 매개 변수로
+            MethodInfo methodInfo = type.GetMethod("Print");
+            PropertyInfo nameProperty = type.GetProperty("Name");
+            PropertyInfo phoneProperty = type.GetProperty("Phone");
+
+            object profile = Activator.CreateInstance(type, "이지훈", "123-4567");
+            methodInfo.Invoke(profile, null);
+
+            profile = Activator.CreateInstance(type);
+            nameProperty.SetValue(profile, "신세경",null);
+            phoneProperty.SetValue(profile, "000-0000",null);
+
+            Console.WriteLine("{0}, {1}",
+                nameProperty.GetValue(profile, null),
+                phoneProperty.GetValue(profile, null));
+        }
+    }
+}
---
+++ CsharpExamples/MainApp_16_1_3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExamples
+{
+    class MainApp_16_1_3
+    {
+        static void Main(string[] args)
+        {
+            AssemblyBuilder newAssembly =
+                AppDomain.CurrentDomain.DefineDynamicAssembly(
+                    new AssemblyName("CsharpExamples"),
+                    AssemblyBuilderAccess.Run);
+
+            ModuleBuilder newModule = newAssembly.DefineDynamicModule("Calculator");
+            TypeBuilder newType = newModule.DefineType("Sum1To100");
+
+            MethodBuilder newMethod = newType.DefineMethod(
+                "Calculate",
+                MethodAttributes.Public, 
+                typeof(int),
+                new Type[0]);
+
+            ILGenerator generator = newMethod.GetILGenerator();
+
+            generator.Emit(OpCodes.Ldc_I4, 1);
+
+            for (int i = 2; i <= 100; i++)
+            {
+                generator.Emit(OpCodes.Ldc_I4, i);
+                generator.Emit(OpCodes.Add);
+            }
+
+            generator.Emit(OpCodes.Ret);
+            newType.CreateType();
+
+            object sum1to100 = Activator.CreateInstance(newType);
+            MethodInfo Calculate = sum1to100.GetType().GetMethod("Calculate");
+            Console.WriteLine(Calculate.Invoke(sum1to100,null));
+        }
+    }
+}
