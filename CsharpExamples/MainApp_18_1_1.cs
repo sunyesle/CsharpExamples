7d51ac8fb01343e9aad39b7123ca13b6656cf282
sunyesle <sunyesle@gmail.com>
2019-01-02 14:24:35 +0900

    ch18 파일 다루기

---
+++ CsharpExamples/MainApp_18_1_1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CsharpExamples
+{
+    class MainApp_18_1_1
+    {
+        static void Main(string[] args)
+        {
+            
+            string directory = "C:\\";
+            
+            //if (args.Length < 1)
+            //    directory = ".";
+            //else
+            //    directory = args[0];
+            
+            Console.WriteLine("{0} directory Info", directory);
+            Console.WriteLine("-Directories :");
+
+            // 하위 디렉토리 목록 조회
+            var directories = (from dir in Directory.GetDirectories(directory)
+                              let info = new DirectoryInfo(dir)
+                              select new
+                              {
+                                  Name = info.Name,
+                                  Attributes = info.Attributes
+                              }).ToList();
+
+            foreach (var d in directories)
+                Console.WriteLine("{0} : {1}", d.Name, d.Attributes);
+
+            Console.WriteLine("-File :");
+            // 하위 파일 목록 조회
+            var files = (from file in Directory.GetFiles(directory)
+                         // let은 LINQ안에서 변수를 만듬
+                         let info = new FileInfo(file)
+                         select new
+                         {
+                             Name = info.Name,
+                             FileSize = info.Length,
+                             Attributes = info.Attributes
+                         });
+            foreach (var f in files)
+                Console.WriteLine("{0} : {1}, {2}",
+                    f.Name, f.FileSize, f.Attributes);
+        }
+    }
+}
---
+++ CsharpExamples/MainApp_18_1_2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CsharpExamples    // 매개변수로 입력받은 경로에 새 디렉토리나 파일을 만듬
+{
+    class MainApp_18_1_2
+    {
+        static void OnWrongType(string type)
+        {
+            Console.WriteLine("{0} is wrong type", type);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage : Touch.exe <Path> [Type:File/Directory]");
+                return;
+            }
+
+            string path = args[0];
+            string type = "File";
+            if (args.Length > 1)
+                type = args[1];
+
+            if(File.Exists(path)||Directory.Exists(path))
+            {
+                if (type == "File")
+                    File.SetLastWriteTime(path, DateTime.Now);
+                else if (type == "Directory")
+                    Directory.SetLastWriteTime(path, DateTime.Now);
+                else
+                {
+                    OnWrongType(path);
+                    return;
+                }
+                Console.WriteLine("Update {0} {1}", path, type);
+            }
+            else
+            {
+                if (type == "File")
+                    File.Create(path).Close();
+                else if (type == "Directory")
+                    Directory.CreateDirectory(path);
+                else
+                {
+                    OnWrongType(path);
+                    return;
+                }
+
+                Console.WriteLine("Create {0} {1}",path, type);
+            }
+        }
+    }
+}
