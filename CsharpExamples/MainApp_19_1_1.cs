9d15b3d4968813acf7c05dd5681a9df62d6c6e49
sunyesle <sunyesle@gmail.com>
2019-01-03 23:36:26 +0900

    ch19 스레드와 태스크

---
+++ CsharpExamples/MainApp_19_1_1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CsharpExamples
+{
+    class MainApp_19_1_1
+    {
+        static void DoSomething()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("DoSomething : {0}", i);
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(DoSomething));
+
+            Console.WriteLine("Starting thread...");
+            t1.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Main : {0}", i );
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine("Waiting until thread stops...");
+            t1.Join();
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
---
+++ CsharpExamples/MainApp_19_1_2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CsharpExamples
+{
+    class SideTask
+    {
+        int count;
+
+        public SideTask(int count)
+        {
+            this.count = count;
+        }
+
+        public void KeepAlive()
+        {
+            try
+            {
+                while(count > 0)
+                {
+                    Console.WriteLine("{0} left", count--);
+                    Thread.Sleep(10);
+                }
+                Console.WriteLine();
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine(e);
+                Thread.ResetAbort();
+            }
+            finally
+            {
+                Console.WriteLine("Clearing resource...");
+            }
+        }
+    }
+
+    class MainApp_19_1_2
+    {
+        static void Main(string[] args)
+        {
+            SideTask task = new SideTask(100);
+            Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+            t1.IsBackground = false;
+
+            Console.WriteLine("Starting thread...");
+            t1.Start();
+
+            Thread.Sleep(100);
+
+            Console.WriteLine("Aborting thread...");
+            t1.Abort();
+
+            Console.WriteLine("Wating until thread stops...");
+            t1.Join();
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
