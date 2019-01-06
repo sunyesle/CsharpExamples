d70e6254fac661b0c6fbcd158e46bf46c7139c61
sunyesle <sunyesle@gmail.com>
2019-01-06 19:57:51 +0900

    ch19 스레드와 태스크

---
+++ CsharpExamples/MainApp_19_3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExamples    // anync한정자와 await연산자
+{
+    class MainApp_19_3
+    {
+        async static private void MyMethodAsync(int count)
+        {
+            Console.WriteLine("C");
+            Console.WriteLine("D");
+
+            await Task.Run(async () =>
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    Console.WriteLine("{0}/{1}...", i, count);
+                    await Task.Delay(100);
+                }
+            });
+
+            Console.WriteLine("G");
+            Console.WriteLine("H");
+        }
+
+        static void Caller()
+        {
+            Console.WriteLine("A");
+            Console.WriteLine("B");
+
+            MyMethodAsync(3);
+
+            Console.WriteLine("E");
+            Console.WriteLine("F");
+        }
+
+        static void Main(string[] args)
+        {
+            Caller();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CsharpExamples/MainApp_19_3_1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExamples    // .NET프레임워크가 제공하는 비동기 API
+{
+    class MainApp_19_3_1
+    {
+        static async Task<long> CopyAsync(string FromPath, string ToPath)
+        {
+            using (
+                var fromStream = new FileStream(FromPath, FileMode.Open))
+            {
+                long totalCopied = 0;
+
+                using (
+                    var toStream = new FileStream(ToPath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[1024];
+                    int nRead = 0;
+                    while ((nRead =
+                        await fromStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                    {
+                        await toStream.WriteAsync(buffer, 0, nRead);
+                        totalCopied += nRead;
+                    }
+                }
+                return totalCopied;
+            }
+        }
+
+        static async void DoCopy (string FromPath, string ToPath)
+        {
+            long totalCopied = await CopyAsync(FromPath, ToPath);
+            Console.WriteLine("Copied Total {0} Bytes." , totalCopied);
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length < 2)
+            {
+                Console.WriteLine("Usage : Async <Source> <Detination>");
+                return;
+            }
+
+            DoCopy(args[0], args[1]);
+
+            Console.ReadLine();
+        }
+    }
+}
