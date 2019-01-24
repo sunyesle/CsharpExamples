7055eff9df14d18504aa1f57c7b01b154d31a977
sunyesle <sunyesle@gmail.com>
2019-01-24 23:58:48 +0900

    ch7 클래스

---
+++ CsharpExamples/ch07_class/MainApp_7_11.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExamples.ch07_class // MethodHiding
+{
+    class Base
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("Base.MyMethod()");
+        }
+    }
+
+    class Derived : Base
+    {
+        // new한정자
+        public new void MyMethod()
+        {
+            Console.WriteLine("Derived.MyMethod()");
+        }
+    }
+
+    class MainApp_7_11
+    {
+        static void Main(string[] args)
+        {
+            Base baseObj = new Base();
+            baseObj.MyMethod();
+
+            Derived derivedObj = new Derived();
+            derivedObj.MyMethod();
+
+            Base baseOrDerived = new Derived();
+            baseOrDerived.MyMethod();
+        }
+    }
+}
---
+++ CsharpExamples/ch07_class/MainApp_7_12.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExamples.ch07_class
+{
+    class Base
+    {
+        public virtual void SealMe()
+        {
+        }
+    }
+
+    class Derived : Base
+    {
+        // sealed 한정자로 overriding 봉인 가능함
+        // virtual로 선언된 가상 메소드를 오버라이딩한 버전의 메소드만이 가능함
+        public sealed override void SealMe()
+        {
+        }
+    }
+
+    class WantToOverride : Derived
+    {
+        // overriding 봉인 됨
+        public override void SealMe()
+        {
+
+        }
+    }
+
+    class MainApp_7_12
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
