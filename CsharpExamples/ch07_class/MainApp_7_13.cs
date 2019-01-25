84a8484a5495d4edcdd4b8e7d2294f165ab6c9c3
sunyesle <sunyesle@gmail.com>
2019-01-25 23:50:19 +0900

    ch7 클래스

---
+++ CsharpExamples/ch07_class/MainApp_7_13.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExamples.ch07_class // NestedClass
+{
+    // 중첩클래스 사용하는 이유
+    // - 클래스 외부에 공개하고 싶지 않은 형식을 만들고자 할 때
+    // - 현재의 클래스의 일부분처럼 표현할 수 있는 클래스를 만들고자 할 때
+
+    class Configuration
+    {
+        List<ItemValue> listConfig = new List<ItemValue>();
+
+        public void SetConfig(string item, string value)
+        {
+            ItemValue iv = new ItemValue();
+            iv.SetValue(this, item, value);
+        }
+
+        public string GetConfig(string item)
+        {
+            foreach (ItemValue iv in listConfig)
+            {
+                if (iv.GetItem() == item)
+                    return iv.GetValue();
+            }
+
+            return "";
+        }
+
+        // private로 선언되었기 때문에 Configuration클래스 밖에서는 보이지 않음
+        private class ItemValue
+        {
+            private string item;
+            private string value;
+
+            public void SetValue(Configuration config, string item, string value)
+            {
+                this.item = item;
+                this.value = value;
+
+                bool found = false;
+                for (int i = 0; i < config.listConfig.Count; i++) // 중첩클래스는 상위 클래스의 맴버에 자유롭게 접근가능
+                {
+                    if (config.listConfig[i].item == item)
+                    {
+                        config.listConfig[i] = this;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found == false)
+                    config.listConfig.Add(this);
+            }
+
+            public string GetItem()
+            {
+                return item;
+            }
+
+            public string GetValue()
+            {
+                return value;
+            }
+        }
+    }
+
+    class MainApp_7_13
+    {
+        static void Main(string[] args)
+        {
+            Configuration config = new Configuration();
+            config.SetConfig("Version", "V 5.0");
+            config.SetConfig("Size", "655,342 KB");
+
+            Console.WriteLine(config.GetConfig("Version"));
+            Console.WriteLine(config.GetConfig("Size"));
+
+            config.SetConfig("Version", "V 5.0.1");
+            Console.WriteLine(config.GetConfig("Version"));
+        }
+    }
+}
---
+++ CsharpExamples/ch07_class/MainApp_7_15.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyExtention;
+
+namespace MyExtention
+{
+    public static class IntegerExtension
+    {
+        public static int Square(this int myInt)
+        {
+            return myInt * myInt;
+        }
+
+        public static int Power(this int myInt , int exponent)
+        {
+            int result = myInt;
+            for (int i = 0; i < exponent; i++)
+                result = result * myInt;
+
+            return result;
+        }
+    }
+}
+
+namespace CsharpExamples.ch07_class 
+{
+    class MainApp_7_15
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}^2 : {1}", 3, 3.Square());
+            Console.WriteLine("{0}^{1} : {2}", 3, 4, 3.Power(4));
+            Console.WriteLine("{0}^{1} : {2}", 2, 10, 2.Power(10));
+        }
+    }
+}
